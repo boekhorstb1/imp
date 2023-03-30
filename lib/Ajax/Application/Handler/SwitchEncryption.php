a3e03627b0d1e5bad1cd03e7a89a21037650574f
Rafael te Boekhorst <boekhorst@b1-systems.de>
2023-03-30 11:19:11 +0000

    trying to leave ajax class SwitchEncryption as a shell

--- lib/Ajax/Application/Handler/SwitchEncryption.php
+++ lib/Ajax/Application/Handler/SwitchEncryption.php
@@ -10,12 +10,11 @@
  * @license   http://www.horde.org/licenses/gpl GPL
  * @package   IMP
  */
-
+use IMP_Prefs_SwitchHandler as SwitchHandler;
 /**
  * Defines AJAX actions used in the IMP alias dialog.
  *
  * @author    Rafael te Boekhorst <boekhorst@b1-systems.de>
- * @author    Michael Slusarz <slusarz@horde.org>
  * @category  Horde
  * @copyright 2012-2017 Horde LLC
  * @license   http://www.horde.org/licenses/gpl GPL
@@ -23,34 +22,19 @@
  */
 class IMP_Ajax_Application_Handler_SwitchEncryption extends Horde_Core_Ajax_Application_Handler
 {
+
     /**
      * AJAX action: Get Keys from Identity of adressbook.
      *
+     * 
      * @return array  Array with keys of identity.
      */
     public function getIdentityPubKey()
     {
-        global $injector, $notification;
-
         // get ID of identity and email
         $identityID = $this->vars->identityID;
-        $identity = $injector->getInstance('IMP_Identity');
-        $email = $identity->getEmail($identityID);
-        $name = $identity->getName($identityID);
 
-        $smime_url = IMP_Basic_Smime::url();
-        $self_url = Horde::url($GLOBALS['registry']->getServiceLink('prefs', 'imp'), false);
-
-        try {
-            $linksToKey = [
-                'view' => Horde::link($smime_url->copy()->add(['actionID' => 'view_public_key', 'email' => $email]), sprintf(_('View %s Public Key'), $name), null, 'view_key'),
-                'info' => Horde::link($smime_url->copy()->add(['actionID' => 'info_public_key', 'email' => $email]), sprintf(_('View %s Public Info'), $name), null, 'info_key'),
-                ];
-        } catch (\Throwable $th) {
-            throw $th;
-        }
-
-
-        return $linksToKey;
+        $handler = new SwitchHandler();
+        return $handler->getPublicKeysForPrefsIdentities($identityID);
     }
 }
---
+++ lib/Prefs/SwitchHandler.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Passes Public Keys to the JS frontend concerning Identityes and Preferencess
+ *
+ * @author    Rafael te Boekhorst <boekhorst@b1-systems.de>
+ * @category  Horde
+ * @copyright 2012-2017 Horde LLC
+ * @license   http://www.horde.org/licenses/gpl GPL
+ * @package   IMP
+ */
+class IMP_Prefs_SwitchHandler
+{
+
+    /**
+     * gets the keys form the address book for the prefs identity frontend
+     * 
+     * @param integer $identityID: the identity from where the keys should be gotten
+     * 
+     * @return array returns an array containing links to the keys
+     */
+    public function getPublicKeysForPrefsIdentities($identityID){
+        global $injector, $registry;
+
+        $identity = $injector->getInstance('IMP_Identity');
+        $email = $identity->getEmail($identityID);
+        $name = $identity->getName($identityID);
+
+        $smime_url = IMP_Basic_Smime::url();
+        $self_url = Horde::url($registry->getServiceLink('prefs', 'imp'), false);
+
+        try {
+            $linksToKey = [
+                'view' => Horde::link($smime_url->copy()->add(['actionID' => 'view_public_key', 'email' => $email]), sprintf(_('View %s Public Key'), $name), null, 'view_key'),
+                'info' => Horde::link($smime_url->copy()->add(['actionID' => 'info_public_key', 'email' => $email]), sprintf(_('View %s Public Info'), $name), null, 'info_key'),
+                ];
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
+
+        return $linksToKey;
+
+    }
+}
