6863abefd9027ab12e3789b379d03280298c18b3
Rafael te Boekhorst <boekhorst@b1-systems.de>
2023-04-24 14:06:30 +0000

    trying to fix autolaod

--- test/Imp/bootstrap.php
+++ test/Imp/bootstrap.php
@@ -1,21 +1,32 @@
 <?php
 
-$candidates = [
-    dirname(__FILE__, 6) . '/vendor/autoload.php',
-    'Horde/Test/Bootstrap.php',
-    dirname(__FILE__, 4) . '/test/lib/Horde/Test/Bootstrap.php',
-    dirname(__FILE__, 1) . '/Autoload.php',
-    dirname(__FILE__, 1) . '/TestCase.php',
-    dirname(__FILE__, 1) .'/Stub/HtmlViewer.php',
-    dirname(__FILE__, 1) . '/Stub/Imap.php',
-    dirname(__FILE__, 1) . '/Stub/ItipRequest.php'
-];
-
-// Cover root case and library case
+$loader = function($candidates) {
+    // Cover root case and library case
 foreach ($candidates as $candidate) {
     if (file_exists($candidate)) {
         require_once $candidate;
     }
 }
+};
 
-Horde_Test_Bootstrap::bootstrap(dirname(__FILE__));
+$candidates1 = [
+    dirname(__FILE__, 6) . '/vendor/autoload.php',
+    dirname(__FILE__, 7) . '/vendor/autoload.php',
+];
+
+$loader($candidates1);
+
+//Horde_Test_Bootstrap::bootstrap(dirname(__FILE__));
+
+$candidates2 = [
+     dirname(__FILE__, 1) . '/TestCase.php',
+    'Horde/Test/Bootstrap.php',
+    dirname(__FILE__, 4) . '/test/lib/Horde/Test/Bootstrap.php',
+    dirname(__FILE__, 1) . '/Autoload.php',
+    dirname(__FILE__, 1) .'/Stub/HtmlViewer.php',
+    dirname(__FILE__, 1) . '/Stub/Imap.php',
+    dirname(__FILE__, 1) . '/Stub/ItipRequest.php'
+];
+
+$loader($candidates2);
+
