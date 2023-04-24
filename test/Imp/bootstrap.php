5b7a776c5e606f778811232df57c09401f09ccff
Rafael te Boekhorst <boekhorst@b1-systems.de>
2023-04-24 15:29:43 +0000

    finally udnerstood how unitests are workign per repo basis

--- test/Imp/bootstrap.php
+++ test/Imp/bootstrap.php
@@ -11,16 +11,19 @@ foreach ($candidates as $candidate) {
 
 $candidates1 = [
     dirname(__FILE__, 6) . '/vendor/autoload.php',
-    dirname(__FILE__, 7) . '/vendor/autoload.php',
+    '/vendor/autoload.php',
+    'Horde/Test/Bootstrap.php'
 ];
+
 
 $loader($candidates1);
 
-//Horde_Test_Bootstrap::bootstrap(dirname(__FILE__));
+Horde_Test_Bootstrap::bootstrap(dirname(__FILE__));
+
 
 $candidates2 = [
      dirname(__FILE__, 1) . '/TestCase.php',
-    'Horde/Test/Bootstrap.php',
+     'Horde/Test/Bootstrap.php',
     dirname(__FILE__, 4) . '/test/lib/Horde/Test/Bootstrap.php',
     dirname(__FILE__, 1) . '/Autoload.php',
     dirname(__FILE__, 1) .'/Stub/HtmlViewer.php',
